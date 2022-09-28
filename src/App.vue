69d5baeb872197ed55da123624fd95bf9ea27362
CristianCeccon <Cristian_ceccon45@hotmail.com>
2022-09-28 15:43:15 -0300

    Card Planta

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <template>
   <v-app>
+    
     <v-main>
+        <div>
+          <v-toolbar dark prominent src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      
+            <v-toolbar-title>Vuetify</v-toolbar-title>
+      
+            <v-spacer></v-spacer>
+      
+            <v-btn icon>
+              <v-icon>mdi-export</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </div>
         <router-view></router-view>
     </v-main>
+    <v-footer v-bind="localAttrs" :padless="padless">
+      <v-card flat tile width="100%" class="red lighten-1 text-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+    
+        <v-divider></v-divider>
+    
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 <script>
---
+++ src/views/PlantasView.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+  <v-toolbar color = A5BE7F>
+    <v-toolbar-title> IFolha</v-toolbar-title>
+    <v-autocomplete v-model="select" :loading="loading" :items="items" :search-input.sync="search" cache-items
+      class="mx-4" flat hide-no-data hide-details label="Pesquisar" solo-inverted ></v-autocomplete>
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <v-container fill-height class="bg-image flex-column flex-sm-row">
+    <v-row>
+      <v-col v-for="(planta, i) in plantas" :key="i" cols="12" sm="6" md="4"  lg="3">
+        <v-card class="pa-2" outlined tile>
+          <v-img class="white--text align-end" height="200px" :src="planta.imagem">
+            <v-card-title>{{planta.nome}}</v-card-title>
+          </v-img>
+          
+          <v-card-subtitle class="pb-0">
+            {{planta.nomeCientifico}}
+          </v-card-subtitle>
+          
+          <v-card-text class="text--primary">
+            <div>Plantada em: </div>
+          
+            <div>{{planta.dataPlantio}}</div>
+          </v-card-text>
+          
+          <v-card-actions>
+            <v-btn color="orange" text>
+              Ver Detalhes
+            </v-btn>
+          </v-card-actions>
+          </v-card>
+      </v-col>
+      
+    </v-row>
+  </v-container>
+  </div>
+</template>
+
+
+<!--<template>
+  <v-container fluid fill-height class="bg-image flex-column flex-sm-row">
+    <v-row>
+      oiasdfadsfasdfasdfasdfadsfadsfasd
+    </v-row>
+  </v-container>
+</template>
+
+-->
+
+<script>
+  import { mdiAccount } from "@mdi/js";
+export default {
+  data() {
+    return {
+      loading: false,
+      items: [],
+      search: null,
+      select: null,
+      plantas: [
+        {
+          nome: "Ipê",
+          nomeCientifico: "Ipe Brasiliensis",
+          dataPlantio: "23/09/1991",
+          imagem: "https://http2.mlstatic.com/D_NQ_NP_912828-MLB28347358604_102018-O.jpg"
+        },
+        {
+          nome: "Cerejeira",
+          nomeCientifico: "Cerejeira Brasiliensis",
+          dataPlantio: "23/09/1991",
+          imagem: ""
+        },
+        {
+          nome: "Aracauria",
+          nomeCientifico: "Aracauria Brasiliensis",
+          dataPlantio: "23/09/1991",
+          imagem: ""
+        },
+        {
+          nome: "Jabuticaba",
+          nomeCientifico: "Jabuticaba Brasiliensis",
+          dataPlantio: "23/09/1991",
+          imagem: ""
+        }
+      ],
+    }
+  },
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val)
+    },
+  },
+  methods: {
+    querySelections(v) {
+      this.loading = true
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.plantas.filter(e => {
+          return (e.nome || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+        })
+        this.loading = false
+      }, 500)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.bg-image {
+  background-image: url("../assets/bg-main.jpg");
+  background-position: center;
+  background-size: cover;
+}
+</style>
